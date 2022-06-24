99035a241d068bec7fedd45c3b30188fbbb13a9a
ClassePsycho <107487164+ClassePsycho@users.noreply.github.com>
2022-06-24 16:37:48 -0700

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ $("startingbuttons2").click(function() {
 $(".startingbuttons1").click(function() {
     $(".page2a").delay(500).fadeIn(3500);
     $(".page2a").fadeOut(2000);
+    $(".page2b").delay(500).fadeIn(3500);
+    $(".page2b").fadeOut(2000);
     $(".page2c").delay(5500).fadeIn(3500);
 
 
