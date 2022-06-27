6c32fd795134c2de1e260a32f7751d967ef6cc02
ClassePsycho <107487164+ClassePsycho@users.noreply.github.com>
2022-06-26 18:45:05 -0700

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -24,7 +24,20 @@ $(".startingbuttons1").click(function() {
 
 $(".checkyourpockets").click(function() {
   $(".page2dchoice").delay(600).fadeOut(2000);
-  $(".page3a-1").delay(4200).fadeIn(3500).delay(3500).fadeOut(2000);
-  $(".page3b-1choices").delay(13000).fadeIn(3500);
+  $(".page3a-1").delay(5000).fadeIn(3500).delay(3500).fadeOut(2000);
+  $(".page3b-1choices").delay(14000).fadeIn(3500);
 });
 /*lighmatchs*/
+$(".lightmatches").click(function() {
+  $(".page3b-1choices").delay(2000).fadeOut(2000);
+  $(".page4a-lightmatches").delay(5000).fadeIn(3500).delay(3500).fadeOut(2000);
+  $(".walldeath").delay(14000).fadeIn(3500).delay(3500).fadeOut(2000);
+  $(".death").delay(26000).fadeIn(3500);
+ });
+/*Looks like someone else wanted a snack too.*/
+$(".lightmatches").click(function() {
+  $(".page3b-1choices").delay(2000).fadeOut(2000);
+  $(".page4a-lightmatches").delay(5000).fadeIn(3500).delay(3500).fadeOut(2000);
+  $(".walldeath").delay(14000).fadeIn(3500).delay(3500).fadeOut(2000);
+  $(".death").delay(26000).fadeIn(3500);
+});
