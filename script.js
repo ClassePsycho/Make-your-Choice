cbbc2eeb0b26bcf55b2bb058c7fcde75a9bf4b16
ClassePsycho <107487164+ClassePsycho@users.noreply.github.com>
2022-06-26 20:03:25 -0700

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -27,17 +27,44 @@ $(".checkyourpockets").click(function() {
   $(".page3a-1").delay(5000).fadeIn(3500).delay(3500).fadeOut(2000);
   $(".page3b-1choices").delay(14000).fadeIn(3500);
 });
+/*A Nightmares are the hell that wait*/
+
 /*lighmatchs*/
 $(".lightmatches").click(function() {
-  $(".page3b-1choices").delay(2000).fadeOut(2000);
-  $(".page4a-lightmatches").delay(5000).fadeIn(3500).delay(3500).fadeOut(2000);
-  $(".walldeath").delay(14000).fadeIn(3500).delay(3500).fadeOut(2000);
-  $(".death").delay(26000).fadeIn(3500);
+  $(".page3b-1choices").delay(600).fadeOut(2000);
+  $(".page4a-lightmatches").delay(5000).fadeIn(3500).delay(4500).fadeOut(2000);
+  $(".walldeath").delay(15000).fadeIn(3500).delay(3500).fadeOut(2000);
+  $(".death").delay(28000).fadeIn(3500);
  });
 /*Looks like someone else wanted a snack too.*/
-$(".lightmatches").click(function() {
-  $(".page3b-1choices").delay(2000).fadeOut(2000);
-  $(".page4a-lightmatches").delay(5000).fadeIn(3500).delay(3500).fadeOut(2000);
-  $(".walldeath").delay(14000).fadeIn(3500).delay(3500).fadeOut(2000);
-  $(".death").delay(26000).fadeIn(3500);
+$(".eattrailmix").click(function() {
+  $(".page3b-1choices").delay(600).fadeOut(2000);
+  $(".eattrailmixend").delay(5000).fadeIn(3500).delay(4500).fadeOut(2000);
+  $(".caretoshare").delay(15000).fadeIn(3500).delay(4500).fadeOut(2000);
+  $(".death").delay(28500).fadeIn(3500);
 });
+/* Time is always ticking*/
+$(".checkknife").click(function() {
+  $(".page3b-1choices").delay(600).fadeOut(2000);
+  $(".youtooktolong").delay(5000).fadeIn(3500).delay(4500).fadeOut(2000);
+  $(".gottolovebeinglate").delay(15000).fadeIn(3500).delay(4500).fadeOut(2000);
+  $(".latemuch").delay(25000).fadeIn(3500).delay(4500).fadeOut(2000);
+  $(".wake").delay(36000).fadeIn(3500);
+});
+/*hell is great*/
+$(".gobacktosleep").click(function() {
+  $(".page2dchoice").delay(600).fadeOut(2000);
+  $(".youdied3yearsago").delay(5000).fadeIn(3500).delay(3500).fadeOut(2000);
+  $(".wake").delay(14000).fadeIn(3500);
+});
+$(".sleepagain").click(function() {
+  $(".wake").delay(600).fadeOut(2000);
+  $(".start").delay(5000).fadeIn(3500);
+});
+$(".tryagain").click(function() {
+  $(".death").delay(600).fadeOut(2000);
+  $(".start").delay(5000).fadeIn(3500);
+});
+
+
+
